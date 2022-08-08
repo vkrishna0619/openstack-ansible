8b486df9266e567adcd11bd7f5127b9a1ea975d7
vkrishna0619 <93936751+vkrishna0619@users.noreply.github.com>
2022-08-08 03:07:47 -0700

    this should be the final commit mesaage and we should see before commit show on what way

--- final.sh
+++ final.sh
@@ -1,2 +1,2 @@
 #commit 1
-#this will delete in 2nd commit
+#git commit final with addded file , let's see how it shows
---
+++ squash.sh
@@ -0,0 +1,2 @@
+#testing squash with 1st commit, this will be later added to single commit
+#testing squash with second commit, this i will change the commit name and will use previous 2 commits to squash to this single on
